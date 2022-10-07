79665367278b90e8801ee59ddaa571c3317b4adb
Bryce Eppler <eppler97@gmail.com>
2022-10-07 12:12:09 -0700

    connected to excel sheet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from openpyxl import load_workbook
+
 
 app = FastAPI()
 
@@ -6,3 +8,14 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/test")
+async def test():
+    # load the excel file, form-submission-book.xlsx, and add a row
+    wb = load_workbook('form-submission-book.xlsx')
+    ws = wb.active
+    ws['A2'] = 'Hello World'
+    wb.save('form-submission-book.xlsx')
+    
+    return {"message":"Success adding row to excel file"}
