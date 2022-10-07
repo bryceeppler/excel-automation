157be6e167c01985c3fc412bfa81648bb691ffee
Bryce Eppler <eppler97@gmail.com>
2022-10-07 15:00:47 -0700

    adding endpoints to fetch sheet names and post new rows

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from fastapi import FastAPI
 from openpyxl import load_workbook
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Entry(BaseModel):
+    site: str
+    name: str
+    status: str
+    date: str
+    comment: str
+    sheet: str
+
 
 
 app = FastAPI()
@@ -7,15 +18,38 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to the API"}
 
 
-@app.get("/test")
-async def test():
-    # load the excel file, form-submission-book.xlsx, and add a row
+@app.get("/worksheets")
+async def worksheets():
     wb = load_workbook('form-submission-book.xlsx')
-    ws = wb.active
-    ws['A2'] = 'Hello World'
+    return wb.sheetnames
+
+
+@app.post("/post-to-sheet")
+async def post_to_sheet(entry : Entry):
+    # load the excel file, form-submission-book.xlsx
+    wb = load_workbook('form-submission-book.xlsx')
+
+    # get the sheet name from the entry
+    sheet_name = entry.sheet
+    ws = wb[sheet_name]
+
+    # get the next available row
+    next_row = ws.max_row + 1
+
+    # add the row
+    ws[f'A{next_row}'] = entry.site
+    ws[f'B{next_row}'] = entry.name
+    ws[f'C{next_row}'] = entry.status
+    ws[f'D{next_row}'] = entry.date
+    ws[f'E{next_row}'] = entry.comment
+
+    # save the excel file
     wb.save('form-submission-book.xlsx')
-    
-    return {"message":"Success adding row to excel file"}
+
+    # close the excel file
+    wb.close()
+
+    return entry
